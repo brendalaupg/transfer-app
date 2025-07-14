dd2114e3c76f20bf19d532fe994d0b199096482b
Brenda Lau <brendalaupg@hotmail.com>
2025-07-14 14:25:42 +0800

    add boilerplate typing for recipient and transactions

--- src/app/types.ts
+++ src/app/types.ts
@@ -2,3 +2,23 @@ export interface AccountState {
     balanceAmount: number
     accountNumber: string
 }
+
+export interface Recipient {
+    id: string
+    accountNumber: string
+    name: string
+}
+
+export interface TransferInfo {
+    amount: number
+    recipiant: string
+    note?: number
+}
+
+export interface Transaction {
+    id: string
+    createdAt: string
+    updatedAt?: string
+    amount: number
+    recipiant: string
+}
