410bcc20341003cb07056d1d9c5077ff0dab5d7c
Brenda Lau <brendalaupg@hotmail.com>
2025-07-15 06:14:46 +0800

    task(contact): add contact list screen

---
+++ src/contacts/screens/ContactListScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet, View } from 'react-native'
+
+const ContactListScreen = () => {
+    return <SafeAreaView></SafeAreaView>
+}
+
+export default ContactListScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+})
