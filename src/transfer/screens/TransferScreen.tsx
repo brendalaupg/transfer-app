2b3a0e30a8b5f42b2237bb15fb8451dd4e2ad95c
Brenda Lau <brendalaupg@hotmail.com>
2025-07-14 17:44:01 +0800

    task(transfer): add recipient input

--- src/transfer/screens/TransferScreen.tsx
+++ src/transfer/screens/TransferScreen.tsx
@@ -1,7 +1,13 @@
 import { useNavigation } from '@react-navigation/native'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import React, { memo } from 'react'
-import { SafeAreaView, ScrollView, StyleSheet } from 'react-native'
+import {
+    SafeAreaView,
+    ScrollView,
+    StyleSheet,
+    KeyboardAvoidingView,
+    Platform,
+} from 'react-native'
 import { TransferStackParamList } from '../types'
 import { Button, TextInput } from 'react-native-paper'
 import Typography from '../../common/Typography'
@@ -19,30 +25,57 @@ const TransferScreen = () => {
         navigate('ReviewTransferScreen')
     }
 
+    const onPressContacts = () => {}
+
+    const renderRecipientInput = () => (
+        <TextInput
+            mode={'outlined'}
+            label={'Recipient (Phone Number)'}
+            numberOfLines={1}
+            keyboardType={'number-pad'}
+            right={
+                <TextInput.Icon
+                    icon={'account-outline'}
+                    onPress={() => onPressContacts()}
+                />
+            }
+        />
+    )
+
     const renderAmountInput = () => (
         <TextInput mode={'outlined'} label={'Amount'} placeholder={'RM0.00'} />
     )
 
     return (
         <SafeAreaView style={styles.container}>
-            <ScrollView
-                style={styles.scrollView}
-                contentContainerStyle={styles.scrollViewContent}
+            <KeyboardAvoidingView
+                style={styles.keyboardAvoidingView}
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 20}
             >
-                <Typography variant={'header'} size={'extra-large'}>
-                    {'Transfer'}
-                </Typography>
-                {renderAmountInput()}
-                <TextInput
-                    mode={'outlined'}
-                    label={'Additional Note (optional)'}
-                    multiline={true}
-                    style={{ minHeight: 150 }}
-                />
-                <Button mode={'contained'} onPress={() => onPressSubmit()}>
-                    {'Transfer'}
-                </Button>
-            </ScrollView>
+                <ScrollView
+                    style={styles.scrollView}
+                    contentContainerStyle={styles.scrollViewContent}
+                    keyboardShouldPersistTaps={'handled'}
+                    keyboardDismissMode={'on-drag'}
+                    showsVerticalScrollIndicator={false}
+                >
+                    <Typography variant={'header'} size={'extra-large'}>
+                        {'Transfer'}
+                    </Typography>
+                    {renderRecipientInput()}
+                    {renderAmountInput()}
+                    <TextInput
+                        mode={'outlined'}
+                        label={'Additional Note (optional)'}
+                        multiline={true}
+                        style={{ minHeight: 150 }}
+                    />
+                    <Button mode={'contained'} onPress={() => onPressSubmit()}>
+                        {'Transfer'}
+                    </Button>
+                </ScrollView>
+            </KeyboardAvoidingView>
         </SafeAreaView>
     )
 }
@@ -51,6 +84,9 @@ export default memo(TransferScreen)
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+    },
+    keyboardAvoidingView: {
         flex: 1,
     },
     scrollView: {
