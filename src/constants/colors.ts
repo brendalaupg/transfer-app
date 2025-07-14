e779e985681f1c9fdaa90182edbbaa8db7ce7786
Brenda Lau <brendalaupg@hotmail.com>
2025-07-14 12:39:39 +0800

    task(theme): app colors constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,20 @@
+export const COLORS = {
+    // Backgrounds
+    backgroundPrimary: '#FFFFFF',
+    backgroundSecondary: '#F4F6FB',
+    // Accents
+    accentPrimary: '#2F01E7',
+    accentSecondary: '#32DDD7',
+    // Text
+    textPrimary: '#1A1A1A',
+    textOnPrimary: '#FFFFFF',
+    textSecondary: '#4F4F4F',
+    textOnSecondary: '#003D3C',
+    textDisabled: '#A8A8A8',
+    // States
+    success: '#28C76F',
+    error: '#F44336',
+    disabled: '#E0E0E0',
+    // Etcs
+    divider: '#E0E0E0',
+}
